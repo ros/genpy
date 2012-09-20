1060d9065ebf83a83e501819687b9863a30107e2
Dave Hershberger <hersh@willowgarage.com>
2012-09-20 14:09:43 -0700

    setup.py shortened to use parse_package_for_distutils().

--- setup.py
+++ setup.py
@@ -1,36 +1,12 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from setuptools import setup
-import sys
-from xml.etree.ElementTree import ElementTree
+from distutils.core import setup
+from catkin.package import parse_package_for_distutils
 
-try:
-    root = ElementTree(None, 'stack.xml')
-    version = root.findtext('version')
-except Exception as e:
-    print('Could not extract version from your stack.xml:\n%s' % e, file=sys.stderr)
-    sys.exit(-1)
+d = parse_package_for_distutils()
+d['packages'] = ['genpy']
+d['package_dir'] = {'': 'src'}
+d['install_requires'] = ['genmsg']
+d['scripts'] = ['scripts/genmsg_py.py', 'scripts/gensrv_py.py']
 
-sys.path.insert(0, 'src')
-
-setup(name = 'genpy',
-      version = version,
-      packages = ['genpy'],
-      package_dir = {'': 'src'},
-      install_requires = ['genmsg'],
-      scripts = ['scripts/genmsg_py.py', 'scripts/gensrv_py.py'],
-      author = "Ken Conley",
-      author_email = "kwc@willowgarage.com",
-      url = "http://www.ros.org/wiki/genpy",
-      download_url = "http://pr.willowgarage.com/downloads/genpy/",
-      keywords = ["ROS"],
-      classifiers = [
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License" ],
-      description = "ROS msg/srv Python generation",
-      long_description = """\
-Library and scripts for generating ROS message data structures in Python.
-""",
-      license = "BSD"
-      )
+setup(**d)
