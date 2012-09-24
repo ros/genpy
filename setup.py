5a632d76287a26049da7657f1e200eaa14d5f6bc
Dirk Thomas <dthomas@willowgarage.com>
2012-09-23 22:58:49 -0700

    updated to latest catkin and fixed installation of scripts

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from catkin_pkg.package import parse_package_for_distutils
 d = parse_package_for_distutils()
 d['packages'] = ['genpy']
 d['package_dir'] = {'': 'src'}
-d['scripts'] = ['scripts/genmsg_py.py', 'scripts/gensrv_py.py']
 d['install_requires'] = ['genmsg']
 
 setup(**d)
