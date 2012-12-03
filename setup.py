8cb2a7fe764f4ec246e2f35dbb2dca56f4cf1b7f
Dirk Thomas <dthomas@willowgarage.com>
2012-12-03 10:31:28 -0800

    fix 'requires' in setup.py files

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from catkin_pkg.package import parse_package_for_distutils
 d = parse_package_for_distutils()
 d['packages'] = ['genpy']
 d['package_dir'] = {'': 'src'}
-d['install_requires'] = ['genmsg']
+d['requires'] = ['genmsg']
 
 setup(**d)
