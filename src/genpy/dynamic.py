640ffd262228e662ee6b1c3f435df0c9960f12f4
Janko Petereit <janko.petereit@gmail.com>
2015-10-01 13:20:43 +0200

    fix: escape dot in regex pattern

--- src/genpy/dynamic.py
+++ src/genpy/dynamic.py
@@ -95,7 +95,7 @@ def _gen_dyn_modify_references(py_text, current_type, types):
         # - remove any import statements
         py_text = py_text.replace("import %s.msg"%pkg, '')
         # - rewrite any references to class
-        py_text = re.sub("(?<!\w)%s.msg.%s(?!\w)"%(pkg, base_type), gen_name, py_text)
+        py_text = re.sub("(?<!\w)%s\.msg\.%s(?!\w)"%(pkg, base_type), gen_name, py_text)
 
     pkg, base_type = genmsg.package_resource_name(current_type)
     gen_name = _gen_dyn_name(pkg, base_type)
