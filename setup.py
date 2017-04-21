6be43619cc8c9d94afc3c5c2cd2f0ece2957ec18
AlexV <asmodehn@gmail.com>
2017-04-21 18:14:55 +0900

    now parsing package.xml

--- setup.py
+++ setup.py
@@ -2,17 +2,66 @@
 
 from setuptools import setup
 
+import os
+
+import xml.etree.ElementTree as ET
+
+
+class XmlNodes(object):
+    """
+    Simple API for xml.etree.ElementTree that collapses similar tags.
+    Specially designed for our usage here.
+    """
+
+    def __init__(self, xml_elems):
+        """Handle one list of xml elements"""
+        self.xml_elems = xml_elems
+
+    def __getattr__(self, item):
+        children = XmlNodes([e for e in self.xml_elems if e.tag == item])
+        if len(children.xml_elems) == 0:
+            raise AttributeError
+        else:
+            return children
+
+    def __call__(self, attrib_name=None):
+        if attrib_name is None:  # return the text
+            return ", ".join([e.text for e in self.xml_elems])
+        else:  # return the values of that attribute
+            return ", ".join([e.attrib.get(attrib_name) for e in self.xml_elems])
+
+    def filter(self, attrib_name, attrib_value):
+        """special function to remove nodes we are not interested in"""
+        return XmlNodes([e for e in self.xml_elems if e.attrib.get(attrib_name) == attrib_value])
+
+
+class PackageXml(XmlNodes):
+    """Extracts data from a package.xml file"""
+    def __init__(self, package_xml_path='package.xml'):
+        tree = ET.parse(package_xml_path)
+        root = tree.getroot()
+        assert root.tag == 'package'
+        # TODO : check package format version
+        super(PackageXml, self).__init__([c for c in root])  # here we get all children already
+
+
+ros_package = PackageXml(os.path.join(os.path.dirname(__file__), 'package.xml'))
+
+ros_package.url.filter('type', 'repository')  # 'genpy'
+
+
+
 setup(
-    name='ros_genpy',
-    version='0.5.10',
-    description='Python ROS message and service generators.',
-    url='http://github.com/ros/genpy',
-    author='Ken Conley, Troy Straszheim, Morten Kjaergaard',
-    maintainer_email='dthomas@osrfoundation.org',
-    license='BSD',
+    name='ros_' + ros_package.name(),
+    version=ros_package.version(),
+    description=ros_package.description(),
+    url=ros_package.url.filter('type', 'repository')(),
+    author=ros_package.author(),
+    maintainer_email=ros_package.maintainer('email'),
+    license=ros_package.license(),
+
     packages=['genpy'],
     package_dir={'': 'src'},
-    install_requires=['ros_genmsg', 'pyyaml'],
-    dependency_links=['git+https://github.com/asmodehn/genmsg.git@setuptools#egg=ros_genmsg']
+    install_requires=['pyyaml'],
 )
 
