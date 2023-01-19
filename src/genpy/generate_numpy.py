c071e04244b15498ec8a473bf82c068e2055ca51
Benjamin Lorenz <benjamin.lorenz@dlr.de>
2023-01-19 12:07:26 +0100

    Changes deprecated numpy types to valid ones.

--- src/genpy/generate_numpy.py
+++ src/genpy/generate_numpy.py
@@ -38,20 +38,20 @@ from . generate_struct import serialize
 
 # maps ros msg types to numpy types
 NUMPY_DTYPE = {
-    'float32': 'numpy.float32',
-    'float64': 'numpy.float64',
-    'bool': 'numpy.bool',
-    'int8': 'numpy.int8',
-    'int16': 'numpy.int16',
-    'int32': 'numpy.int32',
-    'int64': 'numpy.int64',
-    'uint8': 'numpy.uint8',
-    'uint16': 'numpy.uint16',
-    'uint32': 'numpy.uint32',
-    'uint64': 'numpy.uint64',
+    'float32': 'numpy.single',
+    'float64': 'numpy.double',
+    'bool': 'numpy.bool_',
+    'int8': 'numpy.byte',
+    'int16': 'numpy.intc',
+    'int32': 'numpy.int_',
+    'int64': 'numpy.longlong',
+    'uint8': 'numpy.ubyte',
+    'uint16': 'numpy.uintc',
+    'uint32': 'numpy.uint',
+    'uint64': 'numpy.ulonglong',
     # deprecated type
-    'char': 'numpy.uint8',
-    'byte': 'numpy.int8',
+    'char': 'numpy.ubyte',
+    'byte': 'numpy.byte',
     }
 
 
